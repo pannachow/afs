536bd3bec3302f292ecf789c4af7ab55fbcc5c74
pannachow <chowpanna@gmail.com>
2022-05-03 11:40:04 +0200

    add github pages deploy script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# taken from https://cli.vuejs.org/guide/deployment.html#github-pages
+# chmod +x deploy.sh to make it executable
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'pannachow.io' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:pannachow/afs.git master:gh-pages
+
+cd -
